b61575c49e0c7a35ab6689116cf67b0f0e108b42
kajal764 <kajaldw666@gmail.com>
2019-12-07 17:58:26 +0530

    store Array using dictionary

--- ArithmeticOperation.sh
+++ ArithmeticOperation.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -x 
 
-Declare -A Operations
+declare -A Operations
+declare -a OperationArray
 echo "enter three inputs"
 read a
 read b
 read c
-
 Operations["op1"]=$(($a+$(($b*$c))))
 Operations["op2"]=$(($(($a*$b))+$c))
 Operations["op3"]=$(($c+$(($a/$b))))
 Operations["op4"]=$(($(($a%$b))+$c))
-
-
+echo ${!Operations[@]}
+for(( i=1;i<=4;i++ ))
+do
+	OperationArray[$i]=${Operations["op$i"]}
+done
+echo "${OperationArray[@]}"
